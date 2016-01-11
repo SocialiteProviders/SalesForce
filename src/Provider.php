3634c2e25176a03f7df3f342504d4006bb853521
Andy Wendt <andy@awendt.com>
2016-01-11 14:27:12 -0700

    removed some fields from the mapped user since they will be in $user->user

--- src/Provider.php
+++ src/Provider.php
@@ -55,25 +55,10 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'salesforce_id' => $user['user_id'],
-            'salesforce_link' => $user['sub'],
-            'salesforce_username' => $user['preferred_username'],
-            'salesforce_organization_id' => $user['organization_id'],
-            'salesforce_nickname' => $user['nickname'],
             'name' => $user['name'],
-            'first_name' => $user['given_name'],
-            'last_name' => $user['family_name'],
             'email' => isset($user['email']) ? $user['email'] : null,
-            'email_verified' => $user['email_verified'],
             'avatar' => $user['picture'],
-            'thumbnail' => array_get($user, 'photos.thumbnail'),
-            'picture' => $user['picture'],
-            'salesforce_urls' => $user['urls'],
-            'salesforce_address' => $user['address'],
-            'salesforce_active' => $user['active'],
-            'salesforce_user_type' => $user['user_type'],
-            'salesforce_language' => $user['language'],
-            'salesforce_utc_offset' => $user['utcOffset'],
+            'nickname' => $user['nickname'],
         ]);
     }
 
